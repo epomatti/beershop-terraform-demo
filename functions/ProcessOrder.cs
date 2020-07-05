39c99f12245cc0acddd165cb11c9a3ebeaaa106f
Evandro Pomatti <evandro.pomatti@gmail.com>
2020-07-05 15:50:00 -0300

    integrated

--- functions/ProcessOrder.cs
+++ functions/ProcessOrder.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data.SqlClient;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -8,10 +9,27 @@ namespace functions
     public static class ProcessOrder
     {
         [FunctionName("ProcessOrder")]
-        public static void Run([ServiceBusTrigger("orders")]string order, ILogger log)
+        public static void Run([ServiceBusTrigger("sbq-orders")] string order, ILogger log)
         {
-            
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {order}");
+
+            var str = Environment.GetEnvironmentVariable("sqldb_connection");
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                var text = "SELECT 1";
+
+                using (SqlCommand cmd = new SqlCommand(text, conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            Console.WriteLine(reader);
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- functions/README.md
+++ functions/README.md
@@ -4,12 +4,16 @@
 
 ```
 docker pull mcr.microsoft.com/mssql/server
-docker run -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=<YourPassword>' -p 1433:1433 -d mcr.microsoft.com/mssql/server
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=StrongPassword#999' -p 1433:1433 -d mcr.microsoft.com/mssql/server
 ```
 
 ```
 init.sh
 ```
+
+Add service bus connection
+
+Start the function
 
 ```
 func start
