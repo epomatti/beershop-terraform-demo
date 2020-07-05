44407ac0d2d14bc882b254591bdc016dc1574dd2
Evandro Pomatti <evandro.pomatti@gmail.com>
2020-07-05 14:15:32 -0300

    functions

--- infrastructure/main/main.tf
+++ infrastructure/main/main.tf
@@ -44,7 +44,7 @@ resource "azurerm_storage_account" "default" {
 }
 
 
-# # SQL Server
+# SQL Server
 
 resource "azurerm_sql_server" "default" {
   name                         = "sql-beershop-${local.env.suffix}"
@@ -53,6 +53,10 @@ resource "azurerm_sql_server" "default" {
   version                      = "12.0"
   administrator_login          = "beershop"
   administrator_login_password = var.SQLSERVER_ADMIN_PASSWORD
+
+  lifecycle {
+    prevent_destroy = true
+  }
 
   tags = local.env.tags
 }
@@ -63,6 +67,10 @@ resource "azurerm_sql_database" "default" {
   location            = azurerm_resource_group.default.location
   server_name         = azurerm_sql_server.default.name
   edition             = local.env.sqldb_edition
+
+  lifecycle {
+    prevent_destroy = true
+  }
 
   tags = local.env.tags
 }
@@ -80,7 +88,7 @@ resource "azurerm_servicebus_namespace" "default" {
 }
 
 resource "azurerm_servicebus_queue" "orders" {
-  name                = local.env.bus_queue_order_name
+  name                = "sbq-orders"
   resource_group_name = azurerm_resource_group.default.name
   namespace_name      = azurerm_servicebus_namespace.default.name
 }
@@ -88,7 +96,7 @@ resource "azurerm_servicebus_queue" "orders" {
 resource "azurerm_servicebus_queue_authorization_rule" "api" {
   name                = "api-permissions"
   namespace_name      = azurerm_servicebus_namespace.default.name
-  queue_name          = local.env.bus_queue_order_name
+  queue_name          = "sbq-orders"
   resource_group_name = azurerm_resource_group.default.name
 
   listen = false
@@ -99,13 +107,14 @@ resource "azurerm_servicebus_queue_authorization_rule" "api" {
 resource "azurerm_servicebus_queue_authorization_rule" "functions" {
   name                = "functions-permissions"
   namespace_name      = azurerm_servicebus_namespace.default.name
-  queue_name          = local.env.bus_queue_order_name
+  queue_name          = "sbq-orders"
   resource_group_name = azurerm_resource_group.default.name
 
   listen = true
   send   = false
   manage = false
 }
+
 
 # App Service Plans
 
@@ -153,7 +162,7 @@ resource "azurerm_app_service" "api" {
     DOCKER_REGISTRY_SERVER_URL                      = "https://beershop.azurecr.io"
     DOCKER_REGISTRY_SERVER_USERNAME                 = "beershop"
     DOCKER_REGISTRY_SERVER_PASSWORD                 = var.ACR_ADMIN_PASSWORD
-    # application variables
+    # beershop variables
     BEERSHOP_SQLSERVER_PASSWORD                     = var.SQLSERVER_ADMIN_PASSWORD
     BEERSHOP_SERVICEBUS_PRIMARY_CONNECTION_STRING   = azurerm_servicebus_queue_authorization_rule.api.primary_connection_string
     BEERSHOP_SERVICEBUS_SECONDARY_CONNECTION_STRING = azurerm_servicebus_queue_authorization_rule.api.secondary_connection_string
@@ -166,4 +175,56 @@ resource "azurerm_app_service" "api" {
   }
 
   tags = local.env.tags
+}
+
+# Application Insights
+
+resource "azurerm_application_insights" "functions" {
+  name                = "appi-beershop-${local.env.suffix}"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+  application_type    = "other"
+
+  tags = local.tags
+}
+
+
+# Functions
+
+resource "azurerm_function_app" "beershop" {
+  name                       = "func-beershop-${local.env.suffix}"
+  resource_group_name        = azurerm_resource_group.default.name
+  location                   = azurerm_resource_group.default.location
+  app_service_plan_id        = azurerm_app_service_plan.functions.id
+  storage_account_name       = azurerm_storage_account.default.name
+  storage_account_access_key = azurerm_storage_account.default.primary_access_key
+  os_type                    = "linux"
+  version                    = "~3"
+
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY"      = azurerm_application_insights.functions.instrumentation_key
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = false
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://beershop.azurecr.io"
+    "DOCKER_REGISTRY_SERVER_USERNAME"     = "beershop"
+    "DOCKER_REGISTRY_SERVER_PASSWORD"     = var.ACR_ADMIN_PASSWORD
+    #"WEBSITE_RUN_FROM_PACKAGE"            = "ThisWillBeSetToAnURLByAzureDevOpsDeploy", // managed by Azure DevOps (must be not null)
+    #"WEBSITE_ENABLE_SYNC_UPDATE_SITE"     = "true"                                     // managed by Azure DevOps (must be not null)
+    # beershop variables
+    BEERSHOP_SQLSERVER_PASSWORD           = var.SQLSERVER_ADMIN_PASSWORD
+    AzureWebJobsServiceBus                = azurerm_servicebus_queue_authorization_rule.api.primary_connection_string
+  }
+
+  site_config {
+    linux_fx_version = "DOCKER|beershop.azurecr.io/beershop-functions:${local.env.suffix}"
+    always_on        = local.env.functions_alwayson
+  }
+
+  lifecycle {
+    ignore_changes = [
+      app_settings["WEBSITE_RUN_FROM_PACKAGE"],
+      app_settings["WEBSITE_ENABLE_SYNC_UPDATE_SITE"]
+    ]
+  }
+
+  tags = local.tags
 }
