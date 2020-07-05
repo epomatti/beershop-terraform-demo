87933275a65021bad131c18fd8924612ce3e6e5e
Evandro Pomatti <evandro.pomatti@gmail.com>
2020-07-05 00:17:10 -0300

    enterprise

---
+++ infrastructure/enterprise/enterprise.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    tfe = "~> 0.19.0"
+  }
+}
+
+locals {
+  organization = "beershop"
+}
+
+resource "tfe_workspace" "beershop-shared" {
+  count              = 1
+  name               = local.workspaces[${count.index}]
+  organization       = local.organization
+  working_directory  = "infrastructure/enterprise"
+
+  vcs_repo {
+      identifier     = "epomatti/beershop-demo"
+      oauth_token_id = var.OAUTH_TOKEN_ID
+      branch         = local.branches[${count.index}]
+  }
+
+}
