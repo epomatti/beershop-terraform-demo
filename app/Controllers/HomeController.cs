f32d170663e27a973207f16926923db132134b85
epomatti <evandro.pomatti@gmail.com>
2022-03-25 18:55:03 -0300

    update

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -1,31 +1,57 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using app.Models;
+using Microsoft.Extensions.Logging;
+using Beershop.Models;
+using Beershop.Repositories;
 
-namespace app.Controllers;
-
-public class HomeController : Controller
+namespace Beershop.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private OrderRepository _repository;
+        private BeershopContext _context;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger, OrderRepository _repository, BeershopContext _context)
+        {
+            _logger = logger;
+            this._repository = _repository;
+            this._context = _context;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            List<Beer> beers = _context.Beers.ToList();
+            ViewData["Beers"] = beers;
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Orders()
+        {
+            List<Order> orders = _context.Orders.ToList();
+            ViewData["Orders"] = orders;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Buy(BeerOrder beerOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                this._repository.CreateOrder(beerOrder);
+                return RedirectToAction(nameof(Orders));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
+
